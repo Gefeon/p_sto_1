3ac7378a8e0c36b5d244f2850fb854ab49f1aa83
kulabuha <kulabuha@inbox.ru>
2021-11-25 12:10:52 +0300

    swagger v1.1

--- src/main/java/com/javamentor/qa/platform/webapp/configs/WebConfig.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/WebConfig.java
@@ -1,44 +1,13 @@
 package com.javamentor.qa.platform.webapp.configs;
 
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import io.swagger.v3.oas.models.OpenAPI;
-
-import java.util.Collections;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    public ApiInfo apiInfo() {
-        return new ApiInfo(
-                "JM Stack REST API",
-                "JM Stack Swagger REST Open API.",
-                "v1.0.0",
-                "Terms of service",
-                new Contact("JM", "www.example.com", "myeaddress@company.com"),
-                "License of API", "API license URL", Collections.emptyList());
     }
 }
