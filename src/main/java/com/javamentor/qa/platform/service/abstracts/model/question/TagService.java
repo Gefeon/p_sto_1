fcac00e6a7d297f015be5513953356602aca07ed
Maxim Markov <maxxmarkov@gmail.com>
2021-11-25 06:11:27 +0000

    fixed some warnings, added test data for users and roles

---
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/question/TagService.java
@@ -0,0 +1,8 @@
+package com.javamentor.qa.platform.service.abstracts.model.question;
+
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.question.Tag;
+import com.javamentor.qa.platform.service.abstracts.model.ReadWriteService;
+
+public interface TagService extends ReadWriteService<Tag, Long> {
+}
