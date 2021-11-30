c606cd53e5bd5ab9e33e24eda9a61840dd7f657c
kulabuha <kulabuha@inbox.ru>
2021-11-30 12:35:38 +0300

    add header-footer-sidebar templates v2.0

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,13 +1,30 @@
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <h3>I'm header</h3>
+
+      <div class="d-flex flex-wrap align-items-center justify-content-around">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <img src="../static/images/page_template/header-logo.svg" alt="logo">
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2">About</a></li>
+          <li><a href="#" class="nav-link px-2">Products</a></li>
+          <li><a href="#" class="nav-link px-2">For team</a></li>
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 flex-fill">
+          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+        </form>
+
+        <div class="text-end">
+          <button type="button" class="btn btn-primary">Login</button>
+          <button type="button" class="btn btn-primary">Sign-up</button>
+        </div>
+      </div>
+
         `
     }
 }
 
 customElements.define('jm-header', Header)
-
-//How to add:
-// <jm-header></jm-header>
-// <script type="text/javascript" th:src="@{/js/header.js}"></script>
--- src/main/resources/static/js/sidebar.js
+++ src/main/resources/static/js/sidebar.js
@@ -1,13 +1,52 @@
 class Sidebar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <h3>I'm sidebar</h3>
+<div>
+    <a href="/" class="link-dark">
+      <span>Home</span>
+    </a>
+    <ul class="list-unstyled pt-3">
+      <li class="mb-1">
+          PUBLIC
+        <div class="collapse show">
+          <ul class="list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark">Questions</a></li>
+            <li><a href="#" class="link-dark">Tags</a></li>
+            <li><a href="#" class="link-dark">Users</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+          COLLECTIVES
+        <div class="collapse show">
+          <ul class="list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark">Explore Collectives</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+          FIND A JOB
+        <div class="collapse show">
+          <ul class="list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark">Jobs</a></li>
+            <li><a href="#" class="link-dark">Companies</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+          TEAMS
+        <div class="collapse show">
+          <ul class="list-unstyled pb-1 small">
+            <li><a href="#" class="link-dark">Create a free Team</a></li>
+            <li><a href="#" class="link-dark">What is Teams</a></li>
+          </ul>
+        </div>
+      </li>
+      
+    </ul>
+  </div>
         `
     }
 }
 
 customElements.define('jm-sidebar', Sidebar)
-
-//How to add:
-// <jm-sidebar></jm-sidebar>
-// <script type="text/javascript" th:src="@{/js/sidebar.js}"></script>
