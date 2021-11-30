2d58fbf378c17db48b005e5d92736c55bb9a3acf
Maxim-Markov <maxxmarkov@gmail.com>
2021-11-30 15:57:29 +0300

    clear files

--- src/main/java/com/javamentor/qa/platform/service/impl/model/question/QuestionServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/question/QuestionServiceImpl.java
@@ -1,17 +1,10 @@
 package com.javamentor.qa.platform.service.impl.model.question;
 
 import com.javamentor.qa.platform.dao.abstracts.model.question.QuestionDao;
-import com.javamentor.qa.platform.dao.util.SingleResultUtil;
-
 import com.javamentor.qa.platform.models.entity.question.Question;
 import com.javamentor.qa.platform.service.abstracts.model.question.QuestionService;
 import com.javamentor.qa.platform.service.impl.model.ReadWriteServiceImpl;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import java.util.Optional;
 
 @Service
 public class QuestionServiceImpl extends ReadWriteServiceImpl<Question, Long> implements QuestionService {
