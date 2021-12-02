f0f8e721c86dcb8909841e9066aa26d8bcc5e251
Achkasov_alexey <alexeyfaker@gmail.com>
2021-12-02 09:17:29 +0300

    Added NoSuchElementException to .orElseThrow() in AuthenticationResourceController

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AuthenticationResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AuthenticationResourceController.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/auth/token")
@@ -38,7 +39,7 @@ public class AuthenticationResourceController {
                 auth.getAuthorities()
                 .stream()
                 .findFirst()
-                .orElseThrow()
+                .orElseThrow(NoSuchElementException::new)
                 .getAuthority());
         return ResponseEntity.ok(token);
     }
