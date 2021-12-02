d6ff59d9190515da043e5f0f6025ae62d65e8675
Maxim-Markov <maxxmarkov@gmail.com>
2021-12-02 15:59:07 +0300

    add warning about bad credentials

--- src/test/java/com/javamentor/qa/platform/api/controllers/AbstractTestControllerClass.java
+++ src/test/java/com/javamentor/qa/platform/api/controllers/AbstractTestControllerClass.java
@@ -1,7 +1,9 @@
 package com.javamentor.qa.platform.api.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.junit5.api.DBRider;
+import com.javamentor.qa.platform.security.jwt.JwtService;
 import com.javamentor.qa.platform.webapp.configs.JmApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,4 +20,15 @@ public abstract class AbstractTestControllerClass {
 
     @Autowired
     protected MockMvc mockMvc;
+    @Autowired
+    protected ObjectMapper objectMapper;
+    @Autowired
+    protected JwtService jwtService;
+    protected static final String USER_ENTITY = "dataset/auth/resourcecontroller/user_entity.yml";
+    protected static final String ROLE_ENTITY = "dataset/auth/resourcecontroller/role.yml";
+    protected static final String AUTH_URI = "/api/auth/token";
+    protected static final String AUTH_HEADER = "Authorization";
+    protected static final String PREFIX = "Bearer ";
+
+
 }
--- src/test/java/com/javamentor/qa/platform/api/controllers/ResourceAnswerControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/api/controllers/ResourceAnswerControllerTest.java
@@ -2,10 +2,14 @@ package com.javamentor.qa.platform.api.controllers;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.javamentor.qa.platform.models.dto.AuthenticationRequestDto;
+import com.javamentor.qa.platform.models.dto.TokenResponseDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -14,18 +18,29 @@ public class ResourceAnswerControllerTest extends AbstractTestControllerClass{
     private final String url = "/api/user/question/100/answer/100";
 
     @Test
-    @DataSet(value = "dataset/DBAnswerExampleAnotherId.yml", disableConstraints = true)
+    @DataSet(value = {"dataset/DBAnswerExampleAnotherId.yml", USER_ENTITY, ROLE_ENTITY}, disableConstraints = true)
     @ExpectedDataSet(value = "dataset/expected/DBAnswerExampleAnotherId.yml")
     public void deleteAnswerWithIncorrectId_returnBadRequest() throws Exception {
-        ResultActions response = mockMvc.perform(delete(url));
+        AuthenticationRequestDto authDto = new AuthenticationRequestDto("user100@user.ru", "user");
+        TokenResponseDto token = objectMapper.readValue(mockMvc
+                .perform(post(AUTH_URI).content(objectMapper.writeValueAsString(authDto)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(), TokenResponseDto.class);
+
+        ResultActions response = mockMvc.perform(delete(url).header(AUTH_HEADER, PREFIX + token.getToken()));
         response.andExpect(status().isBadRequest());
     }
 
     @Test
-    @DataSet(value = "dataset/DBAnswerExample.yml", disableConstraints = true)
+    @DataSet(value = {"dataset/DBAnswerExample.yml", USER_ENTITY, ROLE_ENTITY}, disableConstraints = true)
     @ExpectedDataSet(value = {"dataset/expected/empty.yml"})
     public void deleteAnswer_returnStatusOk_AnswerDeleted() throws Exception {
-        ResultActions response = mockMvc.perform(delete(url));
+        AuthenticationRequestDto authDto = new AuthenticationRequestDto("user100@user.ru", "user");
+        TokenResponseDto token = objectMapper.readValue(mockMvc
+                .perform(post(AUTH_URI).content(objectMapper.writeValueAsString(authDto)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(), TokenResponseDto.class);
+        ResultActions response = mockMvc.perform(delete(url).header(AUTH_HEADER, PREFIX + token.getToken()));
         response.andExpect(status().isOk());
     }
 
