6ad55d607c5b0382a82eb3e0748c32d76a1c6475
Dmitriy Ilin <id1289@yandex.ru>
2021-12-03 14:40:05 +0300

    Изменено имя класс тестов с конфигами - TestAbstractApi Добавлена настройка добавления пустых полей для тестов

--- src/test/java/com/javamentor/qa/platform/api/config/TestAbstractApi.java
+++ src/test/java/com/javamentor/qa/platform/api/config/TestAbstractApi.java
@@ -12,9 +12,9 @@ import org.springframework.test.web.servlet.MockMvc;
 @DBRider
 @SpringBootTest(classes = JmApplication.class)
 @TestPropertySource(properties = {"spring.config.location = src/test/resources/application-test.properties"})
-@DBUnit(caseSensitiveTableNames = true, cacheConnection = false)
+@DBUnit(caseSensitiveTableNames = true, cacheConnection = false, allowEmptyFields = true)
 @AutoConfigureMockMvc
-public class TestConfig {
+public class TestAbstractApi {
 
     @Autowired
     protected MockMvc mvc;
