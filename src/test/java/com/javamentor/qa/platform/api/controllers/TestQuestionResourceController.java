e50e997f3ee194b468a9854cc8de98d7b11c1147
Maxim-Markov <maxxmarkov@gmail.com>
2021-12-06 17:08:48 +0300

    to dev

---
+++ src/test/java/com/javamentor/qa/platform/api/controllers/TestQuestionResourceController.java
@@ -0,0 +1,210 @@
+package com.javamentor.qa.platform.api.controllers;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.javamentor.qa.platform.api.abstracts.AbstractTestApi;
+import com.javamentor.qa.platform.models.dto.AuthenticationRequestDto;
+import com.javamentor.qa.platform.models.dto.QuestionCreateDto;
+import com.javamentor.qa.platform.models.dto.TagDto;
+import com.javamentor.qa.platform.models.dto.TokenResponseDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.iterableWithSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class TestQuestionResourceController extends AbstractTestApi {
+
+    private final String url = "/api/user/question";
+
+    private static final String USER_ENTITY = "dataset/resourceQuestionController/user_entity.yml";
+    private static final String ROLE_ENTITY = "dataset/resourceQuestionController/role.yml";
+    private static final String QUESTION_ENTITY = "dataset/resourceQuestionController/question.yml";
+    private static final String TAG_ENTITY = "dataset/resourceQuestionController/tag.yml";
+    private static final String QUESTION_HAS_TAG_ENTITY = "dataset/resourceQuestionController/questionHasTag.yml";
+
+    private static final String NEW_QUESTION_ADDED = "dataset/expected/resourceQuestionController/newQuestionAdded.yml";
+    private static final String THREE_TAGS_ADDED = "dataset/expected/resourceQuestionController/threeTagsAdded.yml";
+    private static final String THREE_TAG_QUESTION_LINKS_ADDED = "dataset/expected/resourceQuestionController/threeQuestionHasTagsAdded.yml";
+    private static final String TWO_UNIQUE_TAGS_ADDED = "dataset/expected/resourceQuestionController/twoUniqueIdTagsAdded.yml";
+    private static final String TWO_UNIQUE_TAG_QUESTION_LINKS_ADDED = "dataset/expected/resourceQuestionController/twoQuestionHasTagsAdded.yml";
+    private static final String TWO_EXISTENT_TAGS_ADDED = "dataset/expected/resourceQuestionController/twoExistentIdTagsAdded.yml";
+    private static final String TWO_EXISTENT_TAG_QUESTION_LINKS_ADDED = "dataset/expected/resourceQuestionController/twoExistentIdQuestionHasTagAdded.yml";
+    private static final String AUTH_URI = "/api/auth/token";
+    private static final String AUTH_HEADER = "Authorization";
+    private static final String PREFIX = "Bearer ";
+
+    @Test
+    @DataSet(value = {QUESTION_ENTITY, TAG_ENTITY, QUESTION_HAS_TAG_ENTITY, USER_ENTITY, ROLE_ENTITY}, disableConstraints = true)
+    @ExpectedDataSet(value = {NEW_QUESTION_ADDED, THREE_TAGS_ADDED, THREE_TAG_QUESTION_LINKS_ADDED, USER_ENTITY, ROLE_ENTITY})
+    public void postCorrectData_checkResponse() throws Exception {
+        AuthenticationRequestDto authDto = new AuthenticationRequestDto("user100@user.ru", "user");
+        TokenResponseDto token = objectMapper.readValue(mvc
+                .perform(post(AUTH_URI).content(objectMapper.writeValueAsString(authDto)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(), TokenResponseDto.class);
+
+        QuestionCreateDto questionCreateDto = new QuestionCreateDto();
+        questionCreateDto.setDescription("question description");
+        questionCreateDto.setTitle("question title");
+        List<TagDto> tags = new ArrayList<>();
+        int tagCount = 3;
+        for (int i = 0; i < tagCount; i++) {
+            TagDto tag = new TagDto();
+            tag.setName("tagName" + i);
+            tags.add(tag);
+        }
+        questionCreateDto.setTags(tags);
+
+        ResultActions response = mvc.perform(post(url).header(AUTH_HEADER, PREFIX + token.getToken())
+                .content(objectMapper.writeValueAsString(questionCreateDto))
+                .contentType(MediaType.APPLICATION_JSON));
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(greaterThan(0L), Long.class))
+                .andExpect(jsonPath("$.title").value(questionCreateDto.getTitle()))
+                .andExpect(jsonPath("$.description").isNotEmpty())
+                .andExpect(jsonPath("$.authorId").value(greaterThan(0L), Long.class))
+                .andExpect(jsonPath("$.countAnswer").value(0))
+                .andExpect(jsonPath("$.viewCount").value(0))
+                .andExpect(jsonPath("$.countValuable").value(0))
+                .andExpect(jsonPath("$.listTagDto").value(iterableWithSize(tagCount)))
+                .andExpect(jsonPath("$.listTagDto[0].id").value(greaterThan(0L), Long.class));
+    }
+
+    @Test
+    @DataSet(value = {QUESTION_ENTITY, TAG_ENTITY, QUESTION_HAS_TAG_ENTITY, USER_ENTITY, ROLE_ENTITY}, disableConstraints = true)
+    public void postBlankTitle_getBadRequest() throws Exception {
+        AuthenticationRequestDto authDto = new AuthenticationRequestDto("user100@user.ru", "user");
+        TokenResponseDto token = objectMapper.readValue(mvc
+                .perform(post(AUTH_URI).content(objectMapper.writeValueAsString(authDto)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(), TokenResponseDto.class);
+
+        QuestionCreateDto questionCreateDto = new QuestionCreateDto();
+        questionCreateDto.setDescription("question description");
+        questionCreateDto.setTitle(" ");
+        TagDto tag = new TagDto();
+        tag.setName("tagName");
+        questionCreateDto.setTags(List.of(tag));
+
+        MvcResult result = mvc
+                .perform(post(url).header(AUTH_HEADER, PREFIX + token.getToken())
+                        .content(objectMapper.writeValueAsString(questionCreateDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest()).andReturn();
+        Assertions.assertTrue(MethodArgumentNotValidException.class.isAssignableFrom(result.getResolvedException().getClass()));
+    }
+
+    @Test
+    @DataSet(value = {QUESTION_ENTITY, TAG_ENTITY, QUESTION_HAS_TAG_ENTITY, USER_ENTITY, ROLE_ENTITY}, disableConstraints = true)
+    public void postNullDescription_getBadRequest() throws Exception {
+        AuthenticationRequestDto authDto = new AuthenticationRequestDto("user100@user.ru", "user");
+        TokenResponseDto token = objectMapper.readValue(mvc
+                .perform(post(AUTH_URI).content(objectMapper.writeValueAsString(authDto)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(), TokenResponseDto.class);
+
+        QuestionCreateDto questionCreateDto = new QuestionCreateDto();
+        questionCreateDto.setTitle("title");
+        TagDto tag = new TagDto();
+        tag.setName("tagName");
+        questionCreateDto.setTags(List.of(tag));
+
+        MvcResult result = mvc.perform(post(url).header(AUTH_HEADER, PREFIX + token.getToken())
+                        .content(objectMapper.writeValueAsString(questionCreateDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest()).andReturn();
+        Assertions.assertTrue(MethodArgumentNotValidException.class.isAssignableFrom(result.getResolvedException().getClass()));
+    }
+
+    @Test
+    @DataSet(value = {QUESTION_ENTITY, TAG_ENTITY, QUESTION_HAS_TAG_ENTITY, USER_ENTITY, ROLE_ENTITY}, disableConstraints = true)
+    public void postNullTags_getBadRequest() throws Exception {
+        AuthenticationRequestDto authDto = new AuthenticationRequestDto("user100@user.ru", "user");
+        TokenResponseDto token = objectMapper.readValue(mvc
+                .perform(post(AUTH_URI).content(objectMapper.writeValueAsString(authDto)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(), TokenResponseDto.class);
+
+        QuestionCreateDto questionCreateDto = new QuestionCreateDto();
+        questionCreateDto.setDescription("question description");
+        questionCreateDto.setTitle("title");
+
+        MvcResult result = mvc.perform(post(url).header(AUTH_HEADER, PREFIX + token.getToken())
+                        .content(objectMapper.writeValueAsString(questionCreateDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest()).andReturn();
+        Assertions.assertTrue(MethodArgumentNotValidException.class.isAssignableFrom(result.getResolvedException().getClass()));
+
+    }
+
+    @Test
+    @DataSet(value = {QUESTION_ENTITY, TAG_ENTITY, QUESTION_HAS_TAG_ENTITY, USER_ENTITY, ROLE_ENTITY}, disableConstraints = true)
+    @ExpectedDataSet(value = {NEW_QUESTION_ADDED, TWO_UNIQUE_TAGS_ADDED, TWO_UNIQUE_TAG_QUESTION_LINKS_ADDED, USER_ENTITY, ROLE_ENTITY})
+    public void postTagsWithUniqueId_checkNewTagsAdded() throws Exception {
+        AuthenticationRequestDto authDto = new AuthenticationRequestDto("user100@user.ru", "user");
+        TokenResponseDto token = objectMapper.readValue(mvc
+                .perform(post(AUTH_URI).content(objectMapper.writeValueAsString(authDto)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(), TokenResponseDto.class);
+
+        QuestionCreateDto questionCreateDto = new QuestionCreateDto();
+        questionCreateDto.setDescription("question description");
+        questionCreateDto.setTitle("question title");
+        TagDto tag1 = new TagDto();
+        TagDto tag2 = new TagDto();
+        tag1.setName("tagName0");
+        tag2.setName("tagName1");
+        questionCreateDto.setTags(List.of(tag1, tag2));
+
+        mvc.perform(post(url).header(AUTH_HEADER, PREFIX + token.getToken())
+                        .content(objectMapper.writeValueAsString(questionCreateDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    @DataSet(value = {QUESTION_ENTITY, TAG_ENTITY, QUESTION_HAS_TAG_ENTITY, USER_ENTITY, ROLE_ENTITY}, disableConstraints = true)
+    @ExpectedDataSet(value = {NEW_QUESTION_ADDED, TWO_EXISTENT_TAGS_ADDED, TWO_EXISTENT_TAG_QUESTION_LINKS_ADDED, USER_ENTITY, ROLE_ENTITY})
+    public void postTagsWithExistentId_checkNewTagsAdded() throws Exception {
+        AuthenticationRequestDto authDto = new AuthenticationRequestDto("user100@user.ru", "user");
+        TokenResponseDto token = objectMapper.readValue(mvc
+                .perform(post(AUTH_URI).content(objectMapper.writeValueAsString(authDto)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString(), TokenResponseDto.class);
+
+        QuestionCreateDto questionCreateDto = new QuestionCreateDto();
+        questionCreateDto.setDescription("question description");
+        questionCreateDto.setTitle("question title");
+        TagDto tag1 = new TagDto();
+        TagDto tag2 = new TagDto();
+        tag1.setName("db_architecture"); //existed
+        tag2.setName("tagName1");
+        questionCreateDto.setTags(List.of(tag1, tag2));
+
+        mvc.perform(post(url).header(AUTH_HEADER, PREFIX + token.getToken())
+                        .content(objectMapper.writeValueAsString(questionCreateDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk()).andReturn();
+
+    }
+}
