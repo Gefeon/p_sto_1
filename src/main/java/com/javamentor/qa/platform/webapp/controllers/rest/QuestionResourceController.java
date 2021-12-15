e1f24935b017ccfb7727d53845c52f6041938f37
Maxim-Markov <maxxmarkov@gmail.com>
2021-12-15 15:13:27 +0300

    blank

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
@@ -1,8 +1,12 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
+import com.javamentor.qa.platform.models.dto.PageDto;
 import com.javamentor.qa.platform.models.dto.QuestionCreateDto;
 import com.javamentor.qa.platform.models.dto.QuestionDto;
+import com.javamentor.qa.platform.models.dto.UserDto;
+import com.javamentor.qa.platform.models.entity.question.IgnoredTag;
 import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.question.TrackedTag;
 import com.javamentor.qa.platform.models.entity.question.answer.VoteType;
 import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.models.mapper.QuestionMapper;
@@ -18,17 +22,17 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Api(tags = {SwaggerConfig.QUESTION_CONTROLLER})
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/user/question")
 public class QuestionResourceController {
 
     private final QuestionMapper questionMapper;
@@ -50,7 +54,7 @@ public class QuestionResourceController {
                     content = @Content(mediaType = "application/json", schema = @Schema(implementation = Question.class))),
             @ApiResponse(description = "Received tags, description or title were empty or null", responseCode = "400")
     })
-    @PostMapping("/question")
+    @PostMapping()
     public ResponseEntity<QuestionDto> addQuestion(
             @ApiParam(value = "A JSON object containing question title, description and tags", required = true)
             @Valid @RequestBody final QuestionCreateDto questionCreateDto) {
@@ -65,7 +69,7 @@ public class QuestionResourceController {
                             schema = @Schema(implementation = Long.class))),
             @ApiResponse(description = "Vote for this question already exists", responseCode = "400", content = @Content)
     })
-    @PostMapping("/question/{questionId}/upVote")
+    @PostMapping("/{questionId}/upVote")
     public ResponseEntity<?> upVote(@PathVariable("questionId") Long questionId) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (voteQuestionService.checkIfVoteQuestionDoesNotExist(questionId, user.getId())) {
@@ -81,7 +85,7 @@ public class QuestionResourceController {
                             schema = @Schema(implementation = Long.class))),
             @ApiResponse(description = "Vote for this question already exists", responseCode = "400", content = @Content)
     })
-    @PostMapping("/question/{questionId}/downVote")
+    @PostMapping("/{questionId}/downVote")
     public ResponseEntity<?> downVote(@PathVariable("questionId") Long questionId) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (voteQuestionService.checkIfVoteQuestionDoesNotExist(questionId, user.getId())) {
@@ -89,5 +93,27 @@ public class QuestionResourceController {
                     questionId, VoteType.DOWN_VOTE, user));
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Vote for this question already exists");
+    }
+
+
+    @Operation(summary = "Get page with pagination by users' persist datetime", responses = {
+            @ApiResponse(description = " success", responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Question.class))),
+            @ApiResponse(description = "there isn`t curPage parameter in url or parameters in url are not positives numbers", responseCode = "400")
+    })
+    @GetMapping()
+    public ResponseEntity<?> getAllQuestionDto(
+            @ApiParam(value = "positive number representing number of current page", required = true)
+            @RequestParam @Positive(message = "current page must be positive number") int currPage,
+            @ApiParam(value = "positive number representing number of items to show on page")
+            @RequestParam(required = false, defaultValue = "10") @Positive(message = "items must be positive number") int items,
+            @ApiParam(value = "list of tracked tags attached to question")
+            @RequestParam(required = false) List<TrackedTag> trackedTags,
+            @ApiParam(value = "list of ignored tags attached to question")
+            @RequestParam(required = false) List<IgnoredTag> ignoredTags) {
+        Map<Object, Object> map = new HashMap<>();
+        map.put("class", "AllQuestions");
+        PageDto<QuestionDto> page = userDtoService.getPage(currPage, items, map);
+        return ResponseEntity.ok(page);
     }
 }
