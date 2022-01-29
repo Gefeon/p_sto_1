13e99327c1b4f7d31c5372d662a7f38240b4d09b
Sergei Komissarikhin <Komissarihin.s@yandex.ru>
2022-01-29 08:35:32 +0300

    Tests fixed

--- src/test/java/com/javamentor/qa/platform/api/abstracts/AbstractTestApi.java
+++ src/test/java/com/javamentor/qa/platform/api/abstracts/AbstractTestApi.java
@@ -10,6 +10,7 @@ import com.javamentor.qa.platform.webapp.configs.JmApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -38,10 +39,11 @@ public class AbstractTestApi {
     protected ObjectMapper objectMapper;
     @Autowired
     protected JwtService jwtService;
+    @Autowired
+    protected CacheManager cacheManager;
 
     public String getToken(String username, String password) throws Exception {
         AuthenticationRequestDto authDto = new AuthenticationRequestDto(username, password);
-
         TokenResponseDto token = objectMapper.readValue(mvc
                 .perform(post(AUTH_URI).content(objectMapper.writeValueAsString(authDto)).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
